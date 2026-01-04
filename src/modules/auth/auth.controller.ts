4d9d0ab348b36db4417736693f2f68bd5b61c42d
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2026-01-04 21:22:03 +0400

    feat: add grpc method

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,21 @@
 import { Controller } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { GrpcMethod } from "@nestjs/microservices";
+
+import type {
+  SendOtpRequest,
+  SendOtpResponse,
+} from "@mario-teacinema/contracts/gen/auth";
 
 @Controller()
 export class AuthController {
   public constructor(private readonly authService: AuthService) {}
+
+  @GrpcMethod("AuthService", "sendOtp")
+  public async sendOtp(data: SendOtpRequest): Promise<SendOtpResponse> {
+    console.log(`Incoming OTP request: `, data);
+    return {
+      ok: true,
+    };
+  }
 }
