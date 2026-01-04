ff5775c50ff134171ee1714f75a0b74e1813e032
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2026-01-04 23:11:10 +0400

    feat: nest g res infrastructure/redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "./modules/auth/auth.module";
 import { PrismaModule } from "./infrastructure";
 import { ConfigModule } from "@nestjs/config";
+import { RedisModule } from "./infrastructure/redis/redis.module";
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    RedisModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
