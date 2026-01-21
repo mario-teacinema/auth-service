8ca7e6c0817c20441f9eed38a83823344229d9bc
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2026-01-21 08:23:13 +0400

    feat: add env validation

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { MicroserviceOptions, Transport } from "@nestjs/microservices";
+import { ConfigService } from "@nestjs/config";
+import { AllConfigs } from "@/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService<AllConfigs>);
+
+  const host = config.get("grpc.host", { infer: true });
+  const port = config.get("grpc.port", { infer: true });
+
+  const url = `${host}:${port}`;
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.GRPC,
     options: {
       package: "auth.v1",
       protoPath: "node_modules/@mario-teacinema/contracts/proto/auth.proto",
-      url: "localhost:50051",
+      url,
       loader: {
         keepCase: false,
         longs: String,
