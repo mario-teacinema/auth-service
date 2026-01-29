c27847556e4ac4b2e2ca6ace8b7d7ec797446ae9
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2026-01-29 19:05:37 +0400

    feat: add getAuthUrl method

--- src/modules/telegram/telegram.controller.ts
+++ src/modules/telegram/telegram.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
-import { TelegramService } from './telegram.service';
+import { Controller } from "@nestjs/common";
+import { TelegramService } from "./telegram.service";
+import { GrpcMethod } from "@nestjs/microservices";
+import { TelegramInitResponse } from "@mario-teacinema/contracts/gen/auth";
 
 @Controller()
 export class TelegramController {
-  constructor(private readonly telegramService: TelegramService) {}
+  public constructor(private readonly telegramService: TelegramService) {}
+
+  @GrpcMethod("AuthService", "TelegramInit")
+  public async getAuthUrl(): Promise<TelegramInitResponse> {
+    return this.telegramService.getTelegramAuthUrl();
+  }
 }
--- src/modules/telegram/telegram.service.ts
+++ src/modules/telegram/telegram.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { AllConfigs, telegramEnv } from "@/config";
 
 @Injectable()
-export class TelegramService {}
+export class TelegramService {
+  private readonly BOT_ID: string;
+
+  private readonly BOT_TOKEN: string;
+
+  private readonly BOT_USERNAME: string;
+
+  private readonly REDIRECT_ORIGIN: string;
+
+  public constructor(
+    private readonly configService: ConfigService<AllConfigs>,
+  ) {
+    this.BOT_ID =
+      this.configService.get("telegram.botId", { infer: true }) ?? "";
+    this.BOT_TOKEN =
+      this.configService.get("telegram.botToken", {
+        infer: true,
+      }) ?? "";
+    this.BOT_USERNAME =
+      this.configService.get("telegram.botUserName", {
+        infer: true,
+      }) ?? "";
+    this.REDIRECT_ORIGIN =
+      this.configService.get("telegram.redirectOrigin", {
+        infer: true,
+      }) ?? "";
+  }
+
+  public getTelegramAuthUrl() {
+    const url = new URL("https://oauth.telegram.org/auth");
+    url.searchParams.append("bot_id", this.BOT_ID);
+    url.searchParams.append("origin", this.REDIRECT_ORIGIN);
+    url.searchParams.append("request_access", "write");
+    url.searchParams.append(
+      "return_to",
+      `${this.REDIRECT_ORIGIN}/auth/telegram`,
+    );
+    return { url: url.href };
+  }
+}
